a9dc2be656395bb06839cf77f74fca7dab128681
Robin Gerg <gergrobin@gmail.com>
2023-06-25 13:11:25 +0200

    Handles data from server as expected

--- Client/client_socket.py
+++ Client/client_socket.py
@@ -1,6 +1,7 @@
 
 
 import socket, json, threading
+from Authentication.auth import Authentication
 
 
 class ClientSocket:
@@ -8,21 +9,45 @@ class ClientSocket:
         self.PORT = port
         self.HOST = host
         self.client_socket = None
+        self.auth = Authentication()
 
     def _create_socket(self):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect((self.HOST, self.PORT))
 
     def _receive_data(self):
-        received_data = self.client_socket.recv(1024).decode('utf-8')
-        print("DATA: ", received_data)
+        while True:
+            received_data = self.client_socket.recv(1024)
+            if not received_data:
+                break
+            
+            received_data = self._read_json(received_data.decode('utf-8'))
+            if received_data["Type"] == "Tick":
+                pass
+            elif received_data["Type"] == "Fail":
+                print("\n---------------------------\n"
+                      f"{received_data['Payload'][0]}"
+                      "\n---------------------------")
+                self._send_data()
+
+            else:
+                print("DATA: ", received_data)
 
     def _send_data(self):
-        test_messag = {"Type": "Action", "Strategy": "YOYO"}
-        self.client_socket.send(self._jsonify_data(test_messag).encode('utf-8'))
+        # data = {"Type": "Action", "Strategy": "YOYO"}
+        # self.client_socket.send(self._jsonify_data(data).encode('utf-8'))
+
+        # register req.
+        self.auth._show_logic_if()
+        credentials_payoad = self.auth._receive_user_input()
+        self.client_socket.send(self._jsonify_data(credentials_payoad).encode('utf-8'))
+        credentials_payoad["Payload"] = []
 
     def _jsonify_data(self, data: dict) -> json:
         return json.dumps(data)
+    
+    def _read_json(self, data) -> bool:
+        return json.loads(data)
 
 
 if __name__ == '__main__':
