28bd50c8fa02e33aad52d675c3c73a4bc1b06d35
Robin Gerg <gergrobin@gmail.com>
2023-06-27 11:21:01 +0200

    Generates random pos

--- Fighter/fighter.py
+++ Fighter/fighter.py
@@ -1,11 +1,14 @@
-
+import random
 
 class Fighter:
-    def __init__(self) -> None:
+    def __init__(self, name, strategy) -> None:
         self.health = 10
+        self.name = name
         self.attack = 3
         self.defense = 1
-        self.strategy = []
+        self.strategy = strategy
+        self.pos = ()
+        self._random_pos()
 
     def _attack(self):
         """ Attacks other fighter """
@@ -16,3 +19,9 @@ class Fighter:
 
     def _resurrect_fighter(self):
         pass
+
+    def _random_pos(self):
+        """ Saves a random position for a fighter """
+        x_pos = random.randint(0,5)
+        y_pos = random.randint(0,5)
+        self.pos = (x_pos, y_pos)
