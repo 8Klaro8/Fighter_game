42d83183f5c67c2aa5b11f1932b1e875e4ce6275
Robin Gerg <gergrobin@gmail.com>
2023-07-10 13:22:46 +0200

    Added switches and bonus dmg

--- Fighter/duel_manager.py
+++ Fighter/duel_manager.py
@@ -1,21 +1,32 @@
-
+import random
 
 
 
 class DuelManager:
     def __init__(self, fighters: list) -> None:
         self.actions = ["Fight", "Defend", "Run"]
-        self.events = ["Fighter nearby", "2 fighters nearby", "In corner"]
+        self.events = ["1on1", "1on2", "In corner", "below 50% Hp"]
         self.fighters = fighters
         self.extra_defense = 1
         self.attack_deduct_by_def = 1
         self.miss_chance = 8
+        self.fighter_orig_health = 14
+        self.bonus_attack_range = [1, 1, 1, 1, 1, 1, 2, 2, 2, 3]
+        self.is_duel_done = False
+        self.bonus_dmg_added = False
+        self.fought_this_round = False
+
+    def _get_rand_bonus_dmg(self) -> int:
+        """ Selects a random number as bonus dmg """
+        return random.choice(self.bonus_attack_range)
 
     def process_fight(self):
         """ Applies run and defense action and
         proceses fight """
-        self._process_actions()
-        self._process_duel()
+        if len(self.fighters) > 1:
+            self._process_all_strategies()
+            self._add_bonus_dmg_once()
+            self._process_duel()
 
     def _process_actions(self):
         """ If defense or run is chosen as action,
@@ -35,29 +46,90 @@ class DuelManager:
                         fighter._boost_defense(self.extra_defense, self.attack_deduct_by_def)
                         break
 
+    def _process_fighters_strategies(self, fighter):
+        """ Processes each strategy for the given fighter """
+        fighter._reset_defaults() # reset default values before modify again
+        for strat in fighter.strategy:
+            if strat["Action"] == "Run" and \
+                strat["Event"] == "below 50% Hp":
+                if fighter.health <= self.fighter_orig_health / 2:
+                    fighter._boost_miss_chance(self.miss_chance)
+                    self._print_prompt(f"'{fighter.name}' BELOW 50% - health: {fighter.health}")
+                    
+            elif strat["Action"] == "Defend" and \
+                    strat["Event"] == "In corner":
+                if fighter.pos == (0,0) or \
+                    fighter.pos == (3,0) or \
+                    fighter.pos == (0,3) or \
+                    fighter.pos == (3,3):
+                        fighter._boost_defense(self.extra_defense,
+                                            self.attack_deduct_by_def)
+                        self._print_prompt("IN CORNER")
+                            
+            elif strat["Action"] == "Run" and \
+                    strat["Event"] == "1on2":
+                if len(self.fighters) > 2:
+                    print(f"---1ON2---{fighter.name}")
+                    fighter._boost_miss_chance(self.miss_chance)
+
+    def _print_prompt(self, text):
+        print("\n------------------------\n"
+                f"{text}"
+                "\n------------------------\n")
+
+    def _process_all_strategies(self):
+        """ Processes all fighters strategy """
+        for fighter in self.fighters:
+            self._process_fighters_strategies(fighter)
+
+    def _add_bonus_dmg_once(self):
+        """ Adds and prints bonus dmg per round """
+        for fighter in self.fighters:
+            bonus_dmg_1 = self._get_rand_bonus_dmg()
+            fighter._add_bonus_dmg(bonus_dmg_1)
+            print(f"'{fighter.name}' bonus dmg is: {bonus_dmg_1}")
+        
     def _process_duel(self):
         """ If the fighters meet then process their duel """
-        is_duel_done = False
-        while not is_duel_done:
-            for fighter in self.fighters:
-                for fighter_2 in self.fighters:
-                    if fighter != fighter_2:
+        # self.bonus_dmg_added = False # reset bonus dmg
+        self.fought_this_round = False # reset fight round
+        print("\nNew Fight\n----------------")
+        for fighter in self.fighters:
+            for fighter_2 in self.fighters:
+                if fighter.name != fighter_2.name:
+                    # process each strategy before fight
+                    # self._process_fighters_strategies(fighter)
+                    # self._process_fighters_strategies(fighter_2)
+                    # add bonus dmg if not yet been added
+                    # if not self.bonus_dmg_added:
+                    #     print("---Bonus Dmg---")
+                    #     self._add_bonus_dmg(fighter, fighter_2)
+                    # fight only if not fought this round
+                    if not self.fought_this_round:
                         fighter._attack(fighter_2)
                         fighter_2._attack(fighter)
-                        is_duel_done = True
-                        print("Duel's Done!")
-                        print(f"Fighter '{fighter.name}' - health: {fighter.health}, "
-                                f"attack: {fighter.attack}, "
-                                f"defense: {fighter.defense}, "
-                                f"miss chance: {fighter.miss_chance}")
-                        print(f"Fighter '{fighter_2.name}' health: {fighter_2.health}, "
-                                f"attack: {fighter_2.attack}, "
-                                f"defense: {fighter_2.defense}, "
-                                f"miss chance: {fighter_2.miss_chance}")
-                        if is_duel_done:
-                            break
-                break
-            break
+            self.fought_this_round = True
+
+        self._print_involed_fighters()
+
+    def _add_bonus_dmg(self, fighter, fighter_2):
+        """ Adds and prints bonus dmg per round """
+        bonus_dmg_1 = self._get_rand_bonus_dmg()
+        fighter._add_bonus_dmg(bonus_dmg_1)
+        print(f"'{fighter.name}' bonus dmg is: {bonus_dmg_1}")
+
+        bonus_dmg_2 = self._get_rand_bonus_dmg()
+        fighter_2._add_bonus_dmg(bonus_dmg_2)
+        print(f"'{fighter_2.name}' bonus dmg is: {bonus_dmg_2}")
+
+        self.bonus_dmg_added = True
+
+    def _print_involed_fighters(self):
+        """ Prints out the invloved fighters """
+        print("\n---Fight Result---")
+        for fighter in self.fighters:
+            print(f"'{fighter.name}', health: {fighter.health}")
+        print("----------------\n")
 
 # FIGHTER 1:  [{'Action': 'Fight', 'Event': 'Fighter nearby'}]  
 # FIGHTER 2:  [{'Action': 'Defend', 'Event': 'Fighter nearby'}]
